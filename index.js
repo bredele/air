c1ef547d0d835053d273b8be09fcd199899ce133
bredele <olivier.wietrich@gmail.com>
2014-09-22 20:16:17 -0600

    add saturation vapor pressure amd gas constant for water air

--- index.js
+++ index.js
@@ -3,20 +3,51 @@
  * Module dependencies.
  */
 
-var pressure = require('atmospheric');
+var atmospheric = require('atmospheric');
 
 
 /**
  * Density of air using
  * the ideal gas law.
  * 
- * @param {k} temperature
+ * @param {K} temperature
  * @param {m} altitude
  * @param {Number} humidity (percentage)
  * @param {kg·m−3}
  * @api public
  */
 
-module.exports = function(temperature, altitude, humidity) {
-	return 100 * pressure(altitude || 0) / (287.058 * temperature);
+module.exports = function (temperature, altitude, humidity) {
+	var pressure = atmospheric(altitude || 0);
+	var rh = humidity ? constant(temperature, pressure, humidity) : 287.058;
+	return 100 * pressure / (rh * temperature);
 };
+
+
+/**
+ * Specific gas constant for water air.
+ * 
+ * @param  {K} temperature 
+ * @param  {hPa} pressure 
+ * @param  {%} humidity 
+ * @return {J.K-1.mol-1}
+ * @api private
+ */
+
+function constant(temperature, pressure, humidity) {
+	var psat = sat(temperature - 273.15);
+  return 287.06 / (1 - (humidity * psat/pressure) * (1 - 287.06/461));
+}
+
+
+/**
+ * Saturation vapor pressure.
+ * 
+ * @param  {C} temperature
+ * @return {hPa}
+ * @api private 
+ */
+
+function sat(temperature) {
+	return 611.213 * Math.exp(17.5043 * temperature / (241.2 + temperature));
+}
